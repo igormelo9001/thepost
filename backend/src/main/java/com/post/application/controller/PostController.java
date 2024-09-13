134737480631f5fdba038f09841c718b39423fae
unknown <igormelo9001@gmail.com>
2024-09-13 12:26:26 -0300

    update URLs

--- backend/src/main/java/com/post/application/controller/PostController.java
+++ backend/src/main/java/com/post/application/controller/PostController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://frontend:3000")
 @RestController
 @RequestMapping("/posts")
 public class PostController {
